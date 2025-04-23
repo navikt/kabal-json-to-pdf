bf8e6b8b51cb63d159758c8c26401c58c9b636ea
Andreas Jonsson <andreas.jonsson@nav.no>
2025-04-23 08:46:22 +0200

    Correct wording for `frist`.

--- src/main/kotlin/no/nav/klage/pdfgen/util/StringUtils.kt
+++ src/main/kotlin/no/nav/klage/pdfgen/util/StringUtils.kt
@@ -40,7 +40,7 @@ fun getBehandlingstidText(behandlingstidDate: LocalDate?, behandlingstidUnitType
                     " måneder"
                 }
             }
-        } + " fra du mottok dette brevet"
+        }
     } else {
         throw Exception("Needs date or units and unit type")
     }
