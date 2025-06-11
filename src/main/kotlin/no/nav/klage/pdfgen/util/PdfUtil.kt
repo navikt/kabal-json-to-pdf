d64e5052b45c89a051cae749c17c57624bb8be6b
Andreas Jonsson <andreas.jonsson@nav.no>
2025-06-11 23:03:09 +0200

    Fixed tests and implementation. Almost nothing (when creating pdfs) can be reused it seems. You could have preloaded builders though..

--- src/main/kotlin/no/nav/klage/pdfgen/util/PdfUtil.kt
+++ src/main/kotlin/no/nav/klage/pdfgen/util/PdfUtil.kt
@@ -6,21 +6,22 @@ import com.openhtmltopdf.outputdevice.helper.BaseRendererBuilder
 import com.openhtmltopdf.pdfboxout.PDFontSupplier
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
 import com.openhtmltopdf.svgsupport.BatikSVGDrawer
-import no.nav.klage.pdfgen.Application
-import no.nav.klage.pdfgen.service.PDFGenService
 import org.apache.fontbox.ttf.TTFParser
-import org.apache.pdfbox.io.IOUtils
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.pdmodel.font.PDType0Font
 import org.springframework.core.io.ClassPathResource
 import org.w3c.dom.Document
 import java.io.ByteArrayOutputStream
 
+private val colorprofile = ClassPathResource("/sRGB2014.icc").contentAsByteArray
+private val baseUri = ClassPathResource("/dummy.html").uri.toString()
+
+@Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 fun createPDFA(w3doc: Document): ByteArray {
     ByteArrayOutputStream().use { outputStream ->
         PdfRendererBuilder()
             .apply {
-                for (font in FontConfig.fontsWithTTF) {
+                for (font in fontsWithTTF()) {
                     useFont(
                         font.pdFontSupplier,
                         font.family,
@@ -30,10 +31,10 @@ fun createPDFA(w3doc: Document): ByteArray {
                     )
                 }
             }
-            .useColorProfile(colorProfile)
+            .useColorProfile(colorprofile)
             .useSVGDrawer(BatikSVGDrawer())
             .usePdfAConformance(PdfRendererBuilder.PdfAConformance.PDFA_2_U)
-            .withW3cDocument(w3doc, FontConfig.baseUri)
+            .withW3cDocument(w3doc, baseUri)
             .toStream(outputStream)
             .run()
 
@@ -41,36 +42,30 @@ fun createPDFA(w3doc: Document): ByteArray {
     }
 }
 
-@Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-object FontConfig {
-
-    val baseUri: String = PDFGenService::class.java.getResource("/dummy.html").toExternalForm()
-
-    val fontsWithTTF: List<FontWithSupplier> by lazy {
-        val fonts: Array<FontMetadata> =
-            jacksonObjectMapper().readValue(ClassPathResource("/fonts/config.json").inputStream)
-        fonts.map {
-            getFontWithTTF(font = it)
-        }
-    }
-
-    fun getFontWithTTF(font: FontMetadata): FontWithSupplier {
-        val ttf = TTFParser().parseEmbedded(
-            ClassPathResource("/fonts/${font.path}").inputStream
-        )
-        ttf.isEnableGsub = false
-
-        return FontWithSupplier(
-            family = font.family,
-            weight = font.weight,
-            style = font.style,
-            subset = font.subset,
-            pdFontSupplier = PDFontSupplier(PDType0Font.load(PDDocument(), ttf, font.subset)),
-        )
+private fun fontsWithTTF(): List<FontWithSupplier> {
+    val fonts: Array<FontMetadata> =
+        jacksonObjectMapper().readValue(ClassPathResource("/fonts/config.json").contentAsByteArray)
+    return fonts.map { font ->
+        getFontWithTTF(font = font)
     }
 }
 
-data class FontWithSupplier(
+private fun getFontWithTTF(font: FontMetadata): FontWithSupplier {
+    val ttf = TTFParser().parseEmbedded(
+        ClassPathResource("/fonts/${font.path}").inputStream
+    )
+    ttf.isEnableGsub = false
+
+    return FontWithSupplier(
+        family = font.family,
+        weight = font.weight,
+        style = font.style,
+        subset = font.subset,
+        pdFontSupplier = PDFontSupplier(PDType0Font.load(PDDocument(), ttf, font.subset)),
+    )
+}
+
+private data class FontWithSupplier(
     val family: String,
     val weight: Int,
     val style: BaseRendererBuilder.FontStyle,
@@ -78,9 +73,7 @@ data class FontWithSupplier(
     val pdFontSupplier: PDFontSupplier,
 )
 
-val colorProfile: ByteArray = IOUtils.toByteArray(Application::class.java.getResourceAsStream("/sRGB2014.icc"))
-
-data class FontMetadata(
+private data class FontMetadata(
     val family: String,
     val path: String,
     val weight: Int,
