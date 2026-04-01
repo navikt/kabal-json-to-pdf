8f9c9520ae79c8ec257243fae69c2dc658ddc22f
Andreas Jonsson <andreas.jonsson@nav.no>
2026-04-01 08:06:41 +0200

    Remove deps override since Spring has updated.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,9 @@ plugins {
     val kotlinVersion = "2.3.20"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
-    id("org.springframework.boot") version "4.0.3"
+    id("org.springframework.boot") version "4.0.5"
     idea
 }
-
-// CVE GHSA-72hv-8253-57qq: jackson-core async parser DoS. Remove when Spring has updated.
-extra["jackson-2-bom.version"] = "2.21.1"
-extra["jackson-bom.version"] = "3.1.0"
 
 apply(plugin = "io.spring.dependency-management")
 
